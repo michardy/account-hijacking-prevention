1744ca5e1a3c321aaf998f2404170e28706116e2
Michael Hardy <mhardy17@bayschoolsf.org>
2016-10-16 21:50:29 +0000

    Added file descriptions, motor, & static file handling

--- api.py
+++ api.py
@@ -1,14 +1,15 @@
+# This script compares user data
+# it is also called to intialize modules that hash and compare data
+
 import logging
 
-#log = logging.logger
-
 class receiver():
-	def __init__(self):#initialize db connection
+	def __init__(self):
 		self.__hashers = {}
 		self.__comparers = {}
 		self.__maxscores = {}
 
-	def add_hasher(self, name, fxn): #called on by any data collecting module on startup
+	def add_hasher(self, name, fxn): #called on by any data hashing module on startup
 		#registers a function to hash a type of data
 		self.__hashers[name] = fxn
 
@@ -31,20 +32,10 @@ class receiver():
 			req.write('Err: Could not find a handler for "' + req.get_argument('name') + '"')
 			req.set_status(400)
 
-'''	def run(self, request):
-		name = request.get_argument('name')
-		try:
-			scores[name] = self.__fxnList[name](request, self.__username)
-			return (0)
-		except IndexError:
-			print('Client input error: Could not find handler with name of ' + name + '.  ')
-			return(1)
-'''
-
-	def gTrust(self):
+	def gTrust(self, SID, UID, db):
 		total = 0
 		actmax = 0
-		for i in fxnList.keyes():
+		for i in self.__comparers.keys():
 			if self.__scores[i] > -1:
 				actmax += self.__maxscores[i]
 				total += self.__scores[i]
--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+# This script is the main script
+# It responds to HTTP requests and connects to the server
+
 import tornado.ioloop, tornado.web, motor.motor_tornado, api
 
-rec = api.receiver() #setup mongod interface here
+rec = api.receiver()
 
 class api_submit_data(tornado.web.RequestHandler):
 	def get(self):
@@ -8,15 +11,18 @@ class api_submit_data(tornado.web.RequestHandler):
 
 class api_get_trust(tornado.web.RequestHandler):
 	def get(self):
-		self.write(rec.run(self.get_argument('sessionID'),
-			self.get_argument('userID')))
+		db = self.settings['db']
+		self.write(rec.gTrust(self.get_argument('sessionID'),
+			self.get_argument('userID'), db))
+
+db = motor.motor_tornado.MotorClient().hijackingPrevention
 
 def make_app():
 	return(tornado.web.Application([
 		(r"/api/sub_dat", api_submit_data),
 		(r"/api/get_trust", api_get_trust),
-
-	]))
+		(r"/static/(.*)", tornado.web.StaticFileHandler, {'path': 'static/'})
+	], db=db))
 
 if __name__ == '__main__':
 	app = make_app()
