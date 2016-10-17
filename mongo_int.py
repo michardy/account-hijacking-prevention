29f99006a114be462c06ff26e6dfa214660f6b3b
Michael Hardy <mhardy17@bayschoolsf.org>
2016-10-17 11:26:57 +0000

    Added site search to mongo_int.py

--- mongo_int.py
+++ mongo_int.py
@@ -1,15 +1,24 @@
 # This file handles reading and writing data to the databse
 import datetime
 
+def getSiteByClientKey(key, ref, db):
+	sl = db['siteList']
+	site = sl.find_one({'clientKey':key})
+	return(site['_id'])
+
+def getSiteByServerKey(key, ref, db):
+	sl = db['siteList']
+	site = sl.find_one({'serverKey':key})
+
 def session_update(session, data, type):
 	session[type]['data'] = data
 	curTime = datetime.datetime.utcnow()
 	tDelta = datetime.timedelta(hours=60)
 	session[type]['expireTime'] = curTime + tDelta #set the data to expire in an hour
 
-add_to_session(data, type, session, site, db):
-	ssd = db['sessionData_site-' + site.id]
-	result = ssd.find_one('sessionId':session)
+def addToSession(data, type, session, site, db):
+	ssd = db['sessionData_site-' + site]
+	result = ssd.find_one({'sessionId':session})
 	if result:
 		dataObj = result['data']
 	else:
