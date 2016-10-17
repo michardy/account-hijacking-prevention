528a242311acd1918c6522fa3282d18d69a4931a
Michael Hardy <mhardy17@bayschoolsf.org>
2016-10-17 21:50:48 +0000

    Update ip_adress_checker.py ip checker component

--- ip_adress_checker.py
+++ ip_adress_checker.py
@@ -1,15 +1,19 @@
-import api
 import bcrypt
 import hashlib
+import modules
 
 def hasher(data):
 	initialH = hashlib.sha256(data)
 	salt = bcrypt.gensalt()
 	return(bcrypt.hashpw(initialH, salt))
 
-api.addHasher('ip', hasher)
+modules.receiver.rec.addHasher('ip', hasher)
 
-def comparer(hash, hash):
-	return(hash == hash)
+def comparer(SID, UID, site):
+	ssd = db['sessionData_site-' + site]
+	sud = db['userData_site-' + site]
+	session = ssd.find_one({'sessionID':SID})
+	user = sud.find_one({'userID':UID})
+	return(session['data'] == user['data'])
 
-api.addComparer('ip', comparer)
+modules.receiver.api.addComparer('ip', comparer)
