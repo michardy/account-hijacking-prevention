5c284999a20b2b214d554ea5da9bf9d9ff5293a0
Michael Hardy <mhardy17@bayschoolsf.org>
2017-03-05 23:10:43 -0800

    Fixed fingerprint comparer to actually check against user fingerprint

--- modules/fingerprint.py
+++ modules/fingerprint.py
@@ -14,8 +14,8 @@ rec.rec.addHasher('fingerprint', hasher)
 @gen.coroutine
 def comparer(sid, uid, site, db):
 	session = yield mongo_int.getSession(sid, site, db)
-	user = yield mongo_int.getUserDat(uid, site, db)	
-	return(bcrypt.hashpw(session['fingerprint']['data'].encode('utf-8'), user['fingerprint'][0]) == user['ip'][0])
+	user = yield mongo_int.getUserDat(uid, site, db)
+	return(bcrypt.hashpw(session['fingerprint']['data'].encode('utf-8'), user['fingerprint'][0]) == user['fingerprint'][0])
 
 rec.rec.addComparer('fingerprint', comparer, 1)
 
