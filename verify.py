eac922aaaefdc150a30993cad662f8ca279fb0e1
Michael Hardy <mhardy17@bayschoolsf.org>
2017-03-11 22:55:06 -0800

    Added code creation with fernet
    Still need code checking with fernet

--- verify.py
+++ verify.py
@@ -6,13 +6,16 @@ import user
 import os
 import base64
 from tornado import gen
+from cryptography.fernet import Fernet
+
 
 @gen.coroutine
-def makeCode(uid, sid, site, db):
+def makeCode(uid, sid, site, key, db):
 	"""Make verification code and email to user."""
 	secret = base64.b32encode(os.urandom(10))
+	f = Fernet(key)
 	#email code
-	code = hashlib.sha512(secret + uid + sid + site)
+	code = f.encrypt(secret + uid + sid + site)
 	member = user.User(uid, site, db)
 	yield memeber.read_db()
 	member.code = code
@@ -23,4 +26,5 @@ def valCode(secret, uid, sid, site, db):
 	"""Check the varification code."""
 	member = user.User(uid, site, db)
 	yield memeber.read_db()
+	f = Fernet(key)
 	return(member.code == hashlib.sha512(secret + uid + sid + site))
