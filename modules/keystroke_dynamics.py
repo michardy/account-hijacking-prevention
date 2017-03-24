371df588ace3a3ea46ff740c81cf16fe35d2f732
Michael Hardy <mhardy17@bayschoolsf.org>
2017-03-24 14:47:49 -0700

    Updated keystroke dynamics module with v1 data comparison and hashing

--- modules/keystroke_dynamics.py
+++ modules/keystroke_dynamics.py
@@ -1,5 +1,46 @@
+import bcrypt
 import rec
+from tornado import gen
 
+def reformat(key):
+	"""Re-formats database keys to make MongoDB stop complaining they contain periods"""
+	return(key.replace('.', '_'))
+
+@gen.coroutine
+def hasher(data, key, headers, salt):
+	"""This function provides an initial hasher for collected keyboard flight times."""
+	hashed = {}
+	for k in data.keys():
+		#I know I am a terrible person for using the same salt on all of these.  So I promise I will stop
+		hashed[reformat(k)] = bcrypt.hashpw(str(int(data[k]) % 10).encode('utf-8'), salt)
+		#only once this feature is implemented
+		#issue: security
+	return(hashed)
+
+rec.rec.add_hasher('keystroke_dynamics', hasher)
+
+@gen.coroutine
+def comparer(ses_hash, usr_hash):
+	"""This provides a function to compare initially hashed flight times with doubly hashed stored flight times."""
+	total = 0
+	for k in usr_hash.keys():
+		if k in ses_hash:
+			total += (bcrypt.hashpw(ses_hash[k], usr_hash[k]) == usr_hash)
+	return(total)
+
+rec.rec.add_comparer('keystroke_dynamics', comparer, 1)
+
+def translator(data):
+	"""This provides a function that hashes the flight times a second time with a per user hash."""
+	salt = bcrypt.gensalt()
+	hashed = {}
+	for k in data.keys(): #salt reuse
+		hashed[k] = bcrypt.hashpw(data[k], salt) #issue: security
+	retunr(hashed)
+
+rec.rec.add_translator('keystroke_dynamics', translator)
+
+# JavaScript data collection function
 fxn = '''
 var keyDynKeys = [];
 
