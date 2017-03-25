eb271a24c18ab8f30f0adf68ce1d57d30556afb7
Michael Hardy <michardy@users.noreply.github.com>
2017-03-25 11:06:37 -0700

    Update send.js

--- static/js/send.js
+++ static/js/send.js
@@ -1,5 +1,5 @@
 function send(id, data, ck, sid){
 	var xhttpr = new XMLHttpRequest()
-	xhttpr.open("POST", "http://localhost:8080/api/sub_dat")
+	xhttpr.open("POST", "http://54.202.247.3/api/sub_dat")
 	xhttpr.send(JSON.stringify({'name':id,'data':data,'ck':ck,'sessionID':sid,'site':'test'}))
 }
