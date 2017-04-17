bfb6923d1d578d9d2847539d68c666cc8fcbaa48
Michael Hardy <mhardy17@bayschoolsf.org>
2017-04-16 21:19:56 -0700

    client.py tests

---
+++ tests/test_client.py
@@ -0,0 +1,11 @@
+import pathfix
+pathfix.fixpath()
+
+import hijackingprevention.client as client
+
+def test_add():
+	cli = client.Client()
+	cli.add('fake_fxn_name', 'fake_js_fxn', False)
+	assert cli.fxn_names == ['fake_fxn_name']
+	assert cli.fxns == ['fake_js_fxn']
+	assert cli.async == [False]
