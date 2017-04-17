595f17a536194b612eba0bc1db0b444a0e4aea4d
Michael Hardy <mhardy17@bayschoolsf.org>
2017-04-17 11:21:02 -0700

    Initial tests for keystroke_dynamics module

--- tests/test_modules.py
+++ tests/test_modules.py
@@ -5,26 +5,36 @@ from tornado import gen
 
 import modules.fingerprint as fingerprint
 import modules.ip_address_checker as ip_address_checker
+import modules.keystroke_dynamics as keystroke_dynamics
 
 class Launderer():
 	'''Returns data in a way that circumvents pytest's aversion to yielding'''
-	def __init__(self, hasher):
+	def __init__(self, hasher, data):
 		self.hash = ''
 		self.__hasher = hasher
+		self.data = data
 	@gen.coroutine
 	def invoke_untouchable(self):
 		'''Do what Pytest will not'''
 		salt = b'$2b$12$be22kDNa1.ZduxRmhfJWoO'
-		data = 'fake_data'
 		headers = {'X-Real-IP':'fake_data'}
-		self.hash = yield self.__hasher(data, headers, salt)
+		self.hash = yield self.__hasher(self.data, headers, salt)
 
 def test_fingerprint_hasher():
-	launderer = Launderer(fingerprint.hasher)
+	launderer = Launderer(fingerprint.hasher, 'fake_data')
 	launderer.invoke_untouchable()
 	assert launderer.hash == b'$2b$12$be22kDNa1.ZduxRmhfJWoOmQZBb1hlO1972OO70yZhTpX2o02VN3m'
 
 def test_ip_address_checker_hasher():
-	launderer = Launderer(ip_address_checker.hasher)
+	launderer = Launderer(ip_address_checker.hasher, '')
 	launderer.invoke_untouchable()
 	assert launderer.hash == b'$2b$12$be22kDNa1.ZduxRmhfJWoOmQZBb1hlO1972OO70yZhTpX2o02VN3m'
+
+def test_keystroke_dynamics_hasher():
+	data = {
+		'1.2': '1.3',
+		'1.4': '1.5'
+	}
+	launderer = Launderer(keystroke_dynamics.hasher, data)
+	launderer.invoke_untouchable()
+	assert launderer.hash == {'1_2':'1.3','1_4':'1.5'}
