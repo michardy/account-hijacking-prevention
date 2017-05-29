5907755ff7cda665d3b56444f7753536ee97cab2
Michael Hardy <mhardy17@bayschoolsf.org>
2017-05-29 00:23:23 +0000

    Fix #62 adds cryptograpy's new dependencies to setup.sh

--- setup.sh
+++ setup.sh
@@ -5,6 +5,7 @@ sudo apt-get update
 sudo apt-get install python-virtualenv
 sudo apt-get install mongodb
 sudo apt-get install nginx
+sudo apt-get install build-essential libssl-dev libffi-dev python3-dev #unbreak cryptography module
 virtualenv venv -p python3
 source venv/bin/activate
 pip install --upgrade pip
