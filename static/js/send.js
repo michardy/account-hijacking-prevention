60805906af1b91af9e29adb535656df09e87d885
Michael Hardy <michardy@users.noreply.github.com>
2017-05-28 20:41:50 -0700

    Update send.js urls

--- static/js/send.js
+++ static/js/send.js
@@ -1,5 +1,5 @@
 function hijackingPreventionSend(id, data, ck, sid){
 	var xhttpr = new XMLHttpRequest()
-	xhttpr.open("POST", "https://hijackingprevention.com/api/sub_dat")
+	xhttpr.open("POST", "http://localhost:8080/api/sub_dat")
 	xhttpr.send(JSON.stringify({'name':id,'data':data,'ck':ck,'sid':sid}))
 }
