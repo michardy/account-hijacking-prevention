93d22b7f659de5c0eec11e38fd483d8103a3c359
Michael Hardy <98.michael.hardy@gmail.com>
2020-01-22 21:17:57 -0800

    Fix #133 by removing use of reserved async keyword

--- templates/collect.js
+++ templates/collect.js
@@ -3,12 +3,12 @@
 {% end %}
 
 var collectors = {% raw col_list %};
-var async = {% raw async_list %}
+var callback = {% raw callback_list %}
 
 function hijackingPreventionCollect(){
 	for (var c = 0; c < collectors.length; c++){
 		var res = eval(collectors[c]+'()');
-		if (!async[c]){
+		if (!callback[c]){
 			hijackingPreventionSend(res[0], res[1], hijackingPreventionCK, hijackingPreventionSID);
 		}
 	}
